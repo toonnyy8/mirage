5f0179858a9269d260a7c9ae31d6df6ac04a187a
toonnyy8 <toonnyy8@gmail.com>
2020-05-16 16:10:19 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,7 @@
-import { NFA, NFAElement, regParser } from "./scanner/reg2nfa"
-
-// let nfa = regParser(/as(b*|f)g/)
-let nfa = regParser(/b*bc/)
-
-let mapTable = nfa.reduce((prev, curr, idx) => {
-    return { ...prev, [<symbol>curr.state]: idx }
-}, {})
-
-interface DFAElement {
-    state: symbol,
-    links: Array<{ action: string, next: symbol }>
-}
-
-type DFA = Array<DFAElement>
-
-let f = (nfa: NFA, startSet: Array<Symbol>): [Array<number>, { [action: string]: Array<Symbol> }] => {
-
-    let s1: Array<Symbol> = [...startSet]
-    let s2: Array<number> = []
-    let links: { [action: string]: Array<Symbol> } = {}
+import { regParser } from "./scanner/reg2nfa"
+import { f3 } from "./scanner/nfa2dfa"
 
 
-    do {
-        let s = mapTable[<symbol>s1.pop()]
-        if (nfa[s].action == "") {
-            s1.push(...nfa[s].next)
-        } else if (nfa[s].action != null) {
-            links[nfa[s].action] = links[nfa[s].action] || []
-            links[nfa[s].action] = [...links[nfa[s].action], ...nfa[s].next]
-        }
-        s2.push(s)
-    } while (s1.length != 0)
-
-    s2.sort((a, b) => {
-        return a < b ? -1 : a > b ? 1 : 0
-    })
-    return [s2, links]
-}
-
-let setEq = (setA: Array<number>, setB: Array<number>) => {
-    if (setA.length === setB.length) {
-        return setA.find((v, i) => {
-            return v !== setB[i]
-        }) == undefined ? true : false
-    } else {
-        return false
-    }
-}
-
-let sets = []
-let dfa: DFA = []
-
-let f2 = (startSet: Array<Symbol>) => {
-    let [s, links] = f(nfa, startSet)
-    let state = sets.findIndex((v) => {
-        return setEq(s, v)
-    })
-    let _links = {}
-    if (state == -1) {
-        state = sets.push(s) - 1
-        Object.keys(links).forEach(key => {
-            _links[key] = f2(links[key])
-            f2(links[key])
-        })
-        console.log([state, _links])
-    }
-
-    return state
-}
-console.log(nfa)
-f2([nfa[0].state])
+let dfa = f3(regParser(/as(b*|f)g/))
+// let dfa = f3(regParser(/b*(bc)/))
+console.log(dfa)
--- src/scanner/nfa2dfa.ts
+++ src/scanner/nfa2dfa.ts
@@ -1,2 +1,82 @@
-import { NFA, NFAElement } from "./reg2nfa"
+import { NFA } from "./reg2nfa"
 
+export interface DFAElement {
+    state: number,
+    type: string
+    links: { [action: string]: number }
+}
+
+export type DFA = Array<DFAElement>
+
+let f = (nfa: NFA, mapTable: {}, startSet: Array<Symbol>): [Array<number>, { [action: string]: Array<Symbol> }] => {
+
+    let s1: Array<Symbol> = [...startSet]
+    let s2: Array<number> = []
+    let links: { [action: string]: Array<Symbol> } = {}
+
+
+    do {
+        let s = mapTable[<symbol>s1.pop()]
+        if (nfa[s].action == "") {
+            s1.push(...nfa[s].next)
+        } else if (nfa[s].action != null) {
+            links[nfa[s].action] = links[nfa[s].action] || []
+            links[nfa[s].action] = [...links[nfa[s].action], ...nfa[s].next]
+        }
+        s2.push(s)
+    } while (s1.length != 0)
+
+    s2.sort((a, b) => {
+        return a < b ? -1 : a > b ? 1 : 0
+    })
+    return [s2, links]
+}
+
+let setEq = (setA: Array<number>, setB: Array<number>) => {
+    if (setA.length === setB.length) {
+        return setA.find((v, i) => {
+            return v !== setB[i]
+        }) == undefined ? true : false
+    } else {
+        return false
+    }
+}
+
+let f3 = (nfa: NFA) => {
+
+    let mapTable = nfa.reduce((prev, curr, idx) => {
+        return { ...prev, [<symbol>curr.state]: idx }
+    }, {})
+
+    let sets = []
+    let dfa: DFA = []
+
+    let f2 = (startSet: Array<Symbol>) => {
+        let [s, links] = f(nfa, mapTable, startSet)
+        let state = sets.findIndex((v) => {
+            return setEq(s, v)
+        })
+        let _links: { [action: string]: number } = {}
+        if (state == -1) {
+            state = sets.push(s) - 1
+            Object.keys(links).forEach(key => {
+                _links[key] = f2(links[key])
+                f2(links[key])
+            })
+
+            dfa[state] = (Object.freeze({
+                state: state,
+                type: s[0] == 0 ? "entrance" : s[s.length - 1] == nfa.length - 1 ? "exit" : "",
+                links: _links
+            }))
+        }
+
+        return state
+    }
+
+    f2([nfa[0].state])
+
+    return dfa
+}
+
+export { f3 }
