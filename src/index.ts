5180433a43ac2ee77885ea554080f3ab1d6a2c67
toonnyy8 <toonnyy8@gmail.com>
2020-06-21 18:02:19 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ let num = `(${digit}${digit}*)`
 let id = `((_|${a2z}|${A2Z})(_|${a2z}|${A2Z}|${digit})*)`
 let int = `${num}`
 let float = `(${num}.${num})|(${num}.)|(${num})`
-let assign = `(:=)`
+let assign = `(=)`
 let op1 = `(+|-)`
 let op2 = `(\\*|/|%)`
 let op3 = `(>|<|==|!=|>=|<=)`
@@ -49,8 +49,9 @@ let gap = `(${space}|${comment})`
 // $
 // `
 let source = `
-while(-1||2){
-    
+let a = (b,c)=>{
+    a=1;
+    return a;
 };
 $
 `
@@ -58,16 +59,20 @@ let lex = Lex(
     source,
     [
         { type: "gap", reg: gap },
+        { type: "return", reg: `return` },
+        { type: "let", reg: `let` },
         { type: "if", reg: `if` },
         { type: "else", reg: `else` },
         { type: "for", reg: `for` },
         { type: "while", reg: `while` },
         { type: "EOL", reg: EOL },
         { type: "EOF", reg: EOF },
+        { type: ",", reg: `,` },
         { type: "(", reg: `\\(` },
         { type: ")", reg: `\\)` },
         { type: "{", reg: `{` },
         { type: "}", reg: `}` },
+        { type: "=>", reg: `=>` },
         { type: "assign", reg: assign },
         { type: "float", reg: float },
         { type: "op5", reg: op5 },
@@ -86,12 +91,15 @@ let g = [
     genBNF("<Sections>", ["<Sections>", "<Section>",]),
     genBNF("<Sections>", ["<Section>",]),
 
+    genBNF("<Section>", ["<Declares>", "EOL",]),
     genBNF("<Section>", ["<Assign>", "EOL",]),
     genBNF("<Section>", ["<Exp>", "EOL",]),
     genBNF("<Section>", ["EOL"]),
     genBNF("<Section>", ["<If>",]),
     genBNF("<Section>", ["<While>",]),
-
+    genBNF("<Section>", ["<Func>",]),
+    genBNF("<Section>", ["<Return>", "EOL",]),
+    // if
     genBNF("<If>", ["if", "(", "<Exp>", ")", "{", "<Sections>", "}",]),
     genBNF("<If>", ["if", "(", "<Exp>", ")", "{", "<Sections>", "}", "else", "{", "}",]),
     genBNF("<If>", ["if", "(", "<Exp>", ")", "{", "<Sections>", "}", "else", "<If>",]),
@@ -100,12 +108,34 @@ let g = [
     genBNF("<If>", ["if", "(", "<Exp>", ")", "{", "}", "else", "{", "}",]),
     genBNF("<If>", ["if", "(", "<Exp>", ")", "{", "}", "else", "<If>",]),
     genBNF("<If>", ["if", "(", "<Exp>", ")", "{", "}", "else", "{", "<Sections>", "}",]),
-
+    // while
     genBNF("<While>", ["while", "(", "<Exp>", ")", "{", "<Sections>", "}",]),
     genBNF("<While>", ["while", "(", "<Exp>", ")", "{", "}",]),
-
+    // 函式
+    genBNF("<Func>", ["(", ")", "=>", "{", "}",]),
+    genBNF("<Func>", ["(", ")", "=>", "{", "<Sections>", "}",]),
+    genBNF("<Func>", ["(", "<Args>", ")", "=>", "{", "}",]),
+    genBNF("<Func>", ["(", "<Args>", ")", "=>", "{", "<Sections>", "}",]),
+    // 回傳
+    genBNF("<Return>", ["return", "<Func>",]),
+    genBNF("<Return>", ["return", "<Assign>",]),
+    genBNF("<Return>", ["return", "<Exp>",]),
+    // 參數集
+    genBNF("<Args>", ["id",]),
+    genBNF("<Args>", ["id", ",", "<Args>",]),
+    genBNF("<Args>", ["<Assign>",]),
+    genBNF("<Args>", ["<Assign>", ",", "<Args>",]),
+    // 宣告
+    genBNF("<Declares>", ["let", "<Declare>",]),
+    // 參數集
+    genBNF("<Declare>", ["id",]),
+    genBNF("<Declare>", ["id", ",", "<Declare>",]),
+    genBNF("<Declare>", ["<Assign>",]),
+    genBNF("<Declare>", ["<Assign>", ",", "<Declare>",]),
     // 賦值
+    genBNF("<Assign>", ["id", "assign", "<Assign>",]),
     genBNF("<Assign>", ["id", "assign", "<Exp>",]),
+    genBNF("<Assign>", ["id", "assign", "<Func>",]),
 
     genBNF("<Exp>", ["<Term>",]),
     genBNF("<Exp>", ["<Exp>", "op1", "<Term>",]),
@@ -123,8 +153,6 @@ let g = [
     genBNF("<Factor>", ["op5", "<Factor>",]),
     genBNF("<Factor>", ["id",]),
     genBNF("<Factor>", ["float",]),
-    // genBNF("<Factor>", ["op1", "id",]),
-    // genBNF("<Factor>", ["op1", "float",]),
     genBNF("<Factor>", ["(", "<Exp>", ")",]),
 
 ]
@@ -143,11 +171,52 @@ do {
 let syntaxTree = yacc()
 console.log(syntaxTree)
 
+let varTable: Array<{ [name: string]: { type: "float" | "func", isArgs: boolean } }> = [{}]
+let funcScope: Array<number> = []
+let funcCode: Array<string> = []
+interface typeVariable {
+    name: string,
+    type: "float" | typeFuncTypes,
+}
+let genVariable = (name: string, type: "float" | typeFuncTypes,): typeVariable => {
+    return {
+        name,
+        type,
+    }
+}
+interface typeFuncTypes {
+    params: Array<typeVariable>,
+    result: typeVariable,
+}
+let genFuncType = (params: Array<typeVariable>, result: typeVariable = null,): typeFuncTypes => {
+    return {
+        params,
+        result,
+    }
+}
+interface typeFunc {
+    scope: Array<number>,
+    funcType: typeFuncTypes,
+    vars: Array<typeVariable>,
+    code: string,
+}
+let genFunc = (scope: Array<number>, funcType: typeFuncTypes, vars: Array<typeVariable> = [], code: string = "",): typeFunc => {
+    return {
+        scope,
+        funcType,
+        vars,
+        code,
+    }
+}
+let funcs: Array<typeFunc>
 let grammarFunc = {
     "<Program>": (sub: Array<typeSyntaxNode>) => {
+        funcs = [genFunc([0], genFuncType([]))]
+        varTable = [{}]
+        funcScope = [0]
         let out: string = ""
         out += grammarFunc[sub[0].token.type](sub[0].sub)
-
+        funcCode[0] = out
         return out
     },
     "<Sections>": (sub: Array<typeSyntaxNode>) => {
@@ -237,8 +306,120 @@ let grammarFunc = {
 
         return out
     },
+    "<Func>": (sub: Array<typeSyntaxNode>) => {
+        let head: string = ""
+        let args: string = ""
+        let variables: string = ""
+        let main: string = ""
+        let func: typeFunc
+        varTable = [...varTable, {}]
+        funcScope = [...funcScope, varTable.length - 1]
+
+        head += `func $f${varTable.length - 1}`
+        if (sub[1].token.type == "<Args>") {
+            grammarFunc[sub[1].token.type](sub[1].sub)
+            if (sub[5].token.type == "<Sections>") {
+                main += grammarFunc[sub[5].token.type](sub[5].sub)
+            }
+        } else {
+            if (sub[4].token.type == "<Sections>") {
+                main += grammarFunc[sub[4].token.type](sub[4].sub)
+            }
+        }
+        let vt = varTable[funcScope.slice(-1)[0]]
+        args += Object.keys(vt)
+            .reduce((args, name) => {
+                if (vt[name].isArgs == true) {
+                    switch (vt[name].type) {
+                        case "float": {
+                            args += ` (param $${name} f32)`
+                            break
+                        }
+                        case "func": {
+                            break
+                        }
+                    }
+                }
+                return args
+            }, args)
+        variables += Object.keys(vt)
+            .reduce((variables, name) => {
+                if (vt[name].isArgs == false) {
+                    switch (vt[name].type) {
+                        case "float": {
+                            variables += ` (local $${name} f32)`
+                            break
+                        }
+                        case "func": {
+                            break
+                        }
+                    }
+                }
+                return variables
+            }, variables)
+        funcCode[funcScope.slice(-1)[0]] = `(${head}\n${args}\n${variables}\n${main})\n`
+        funcScope = funcScope.slice(0, -1)
+        return ""
+    },
+    "<Args>": (sub: Array<typeSyntaxNode>) => {
+        let out: string = ""
+
+        switch (sub[0].token.type) {
+            case "id": {
+                varTable[funcScope.slice(-1)[0]][sub[0].token.value] = { type: "float", isArgs: true }
+                break
+            }
+            case "<Assign>": {
+                if (sub[0].sub[2].token.type == "<Func>") {
+                    varTable[funcScope.slice(-1)[0]][sub[0].sub[0].token.value] = { type: "func", isArgs: true }
+                } else {
+                    varTable[funcScope.slice(-1)[0]][sub[0].sub[0].token.value] = { type: "float", isArgs: true }
+                }
+                out += grammarFunc[sub[0].token.type](sub[0].sub)
+                break
+            }
+        }
+        if (sub.length == 3) {
+            out += grammarFunc[sub[2].token.type](sub[2].sub)
+        }
+        return out
+    },
+    "<Return>": (sub: Array<typeSyntaxNode>) => {
+
+    },
+    "<Declares>": (sub: Array<typeSyntaxNode>) => {
+        let out: string = ""
+        out += grammarFunc[sub[1].token.type](sub[1].sub)
+        return out
+    },
+    "<Declare>": (sub: Array<typeSyntaxNode>) => {
+        let out: string = ""
+
+        switch (sub[0].token.type) {
+            case "id": {
+                if (varTable[funcScope.slice(-1)[0]][sub[0].token.value]) console.error(`${sub[0].token.value} repeat declaration`)
+                varTable[funcScope.slice(-1)[0]][sub[0].token.value] = { type: "float", isArgs: false }
+                break
+            }
+            case "<Assign>": {
+                if (varTable[funcScope.slice(-1)[0]][sub[0].sub[0].token.value]) console.error(`${sub[0].sub[0].token.value} repeat declaration`)
+                if (sub[0].sub[2].token.type == "<Func>") {
+                    varTable[funcScope.slice(-1)[0]][sub[0].sub[0].token.value] = { type: "func", isArgs: false }
+                } else {
+                    varTable[funcScope.slice(-1)[0]][sub[0].sub[0].token.value] = { type: "float", isArgs: false }
+                }
+                out += grammarFunc[sub[0].token.type](sub[0].sub)
+                break
+            }
+        }
+        if (sub.length == 3) {
+            out += grammarFunc[sub[2].token.type](sub[2].sub)
+        }
+        return out
+    },
     "<Assign>": (sub: Array<typeSyntaxNode>) => {
         let out: string = ""
+        if (varTable[funcScope.slice(-1)[0]][sub[0].token.value] == undefined) console.error(`${sub[0].token.value} is undefined`)
         out += grammarFunc[sub[2].token.type](sub[2].sub)
         out += `set_local $${sub[0].token.value}\n`
 
@@ -372,6 +553,7 @@ let grammarFunc = {
             case 1: {
                 switch (sub[0].token.type) {
                     case "id": {
+                        if (varTable[funcScope.slice(-1)[0]][sub[0].token.value] == undefined) console.error(`${sub[0].token.value} is undefined`)
                         out += `get_local $${sub[0].token.value}\n`
                         break
                     }
@@ -415,4 +597,13 @@ console.log(
 )
 console.log(
     grammarFunc[syntaxTree.token.type](syntaxTree.sub)
+)
+console.log(
+    varTable
+)
+console.log(
+    funcScope
+)
+console.log(
+    funcCode
 )
